74b68c64067902999dcc5df158a459a9f0737403
resty-ShiftLeft <resty@theshiftleft.com>
2024-01-22 16:43:30 +0800

    Add rainfall id stationid readings controller

---
+++ Controllers/RainfallController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAPIRainfall.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreAPIRainfall.Controllers
+{
+    public class RainfallController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public RainfallController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet("/rainfall/id/{stationId}/readings")]
+        public async Task<IActionResult> rainfall(string stationId, [FromQuery] int count = 10)
+        {
+            var externalUrl = "https://environment.data.gov.uk/flood-monitoring/id/stations/" + stationId + "/readings?_sorted&_limit=" + count;
+            var response = await _httpClient.GetStringAsync(externalUrl);
+
+            return Ok(response);
+        }
+    }
+}
