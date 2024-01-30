e9a94cbc45ae194ba40088fbd4602f742688547d
resty-ShiftLeft <resty@theshiftleft.com>
2024-01-30 13:58:25 +0800

    Remove duplicate controller and update responses

--- Controllers/RainfallController.cs
+++ Controllers/RainfallController.cs
@@ -20,159 +20,118 @@ namespace NetCoreAPIRainfall.Controllers
         }
 
         [HttpGet("/rainfall/id/{stationId}/readings")]
+        [SwaggerResponse(200, "A list of rainfall readings successfully retrieved", typeof(List<Models.rainfallReadingResponse>))]
+        [SwaggerResponse(400, "Invalid request", typeof(List<error>))]
+        [SwaggerResponse(404, "No readings found for the specified stationId", typeof(List<error>))]
+        [SwaggerResponse(500, "Internal server error", typeof(List<error>))]
         public async Task<IActionResult> rainfall(string stationId, [FromQuery] int count = 10)
         {
-            var externalUrl = "https://environment.data.gov.uk/flood-monitoring/id/stations/" + stationId + "/readings?_sorted&_limit=" + count;
-            var response = await _httpClient.GetStringAsync(externalUrl);
-
-            var jsonObject_response = JsonConvert.DeserializeObject<JObject>(response);
-            var items = jsonObject_response["items"];
-
-
-            var _readings_res = new List<Models.rainfallReadingResponse>();
-
-            foreach (var item in items)
+            try
             {
-                _readings_res.Add(new Models.rainfallReadingResponse
+                var externalUrl = $"https://environment.data.gov.uk/flood-monitoring/id/stations/{stationId}/readings?_sorted&_limit={count}";
+                using (HttpClient client = new HttpClient())
                 {
-                    Readings = new List<Models.rainfallReading>()
+                    try
                     {
-                        new Models.rainfallReading
+                        HttpResponseMessage message = await client.GetAsync(externalUrl);
+
+                        if (!message.IsSuccessStatusCode)
                         {
-                            dateMeasured = DateTime.Parse(item["dateTime"].ToString()),
-                            amountMeasured = decimal.Parse(item["value"].ToString()),
+                            var customError = new List<error>
+                            {
+                                new error
+                                {
+                                    message = ((int)message.StatusCode).ToString(),
+                                    detail = new List<errorDetail>
+                                    {
+                                        new errorDetail
+                                        {
+                                            propertyName = message.StatusCode.ToString(),
+                                            message = message.IsSuccessStatusCode ? string.Empty : "Details of invalid request property"
+                                        }
+                                    }
+                                }
+                            };
+
+                            return message.StatusCode == System.Net.HttpStatusCode.NotFound
+                                ? NotFound(customError)
+                                : message.StatusCode == System.Net.HttpStatusCode.BadRequest
+                                    ? BadRequest(customError)
+                                    : new JsonResult(customError);
                         }
                     }
-                });
+                    catch (HttpRequestException ex)
+                    {
+                        var customError = new List<error>
+                        {
+                            new error
+                            {
+                                message = string.Empty,
+                                detail = new List<errorDetail>
+                                {
+                                    new errorDetail
+                                    {
+                                        propertyName = string.Empty,
+                                        message =  $"HTTP request error: {ex.Message}",
+                                    }
+                                }
+                            }
+                        };
+                        Console.WriteLine($"HTTP request error: {ex.Message}");
+                        return new JsonResult(customError);
+                    }
+                }
+
+                var response = await _httpClient.GetStringAsync(externalUrl);
+                var items = JsonConvert.DeserializeObject<JObject>(response)?["items"];
+
+                if (items == null || items.Count() == 0)
+                {
+                    var notFoundError = CreateError("404", $"No readings found for the specified stationId {stationId}");
+                    return NotFound(notFoundError);
+                }
+
+                var readingsRes = items?.Select(item => new Models.rainfallReadingResponse
+                {
+                    Readings = new List<Models.rainfallReading>
+            {
+                new Models.rainfallReading
+                {
+                    dateMeasured = DateTime.Parse(item["dateTime"].ToString()),
+                    amountMeasured = decimal.Parse(item["value"].ToString()),
+                }
+            }
+                }).ToList() ?? new List<Models.rainfallReadingResponse>();
+
+                return new JsonResult(readingsRes);
+            }
+            catch (Exception ex)
+            {
+                var error = CreateError("500", "Internal server error", ex.Message);
+                return new JsonResult(error);
             }
 
-
-            return new JsonResult(_readings_res);
         }
 
-
-        /// <summary>
-        /// Get rainfall readings by station Id
-        /// </summary>
-        /// <remarks>
-        /// Retrieve the latest readings for the specified stationId
-        /// </remarks>
-        /// <param name="stationId">The ID of the station for which to retrieve readings.</param>
-        /// 
-
-        [SwaggerResponse(200, "A list of rainfall readings successfully retrieved", typeof(Models.Responses.rainfallReadingResponse))]
-        [SwaggerResponse(400, "Invalid request", typeof(error))]
-        [SwaggerResponse(404, "No readings found for the specified stationId", typeof(error))]
-        [SwaggerResponse(500, "Internal server error", typeof(error))]
-        [HttpGet("/get-rainfall")]
-        public async Task<IActionResult> get_rainfall_by_stationId([Required] string stationId)
+        private List<error> CreateError(string errorCode, string errorMessage, string detail = null)
         {
-            var externalUrl = "https://environment.data.gov.uk/flood-monitoring/id/stations/" + stationId + "/readings?_sorted";
-            var response = await _httpClient.GetStringAsync(externalUrl);
-
-            var jsonObject_response = JsonConvert.DeserializeObject<JObject>(response);
-            var items = jsonObject_response["items"];
-
-            var _readings_res = new List<Models.rainfallReadingResponse>();
-            var _custom_error = new List<error>();
-            using (HttpClient client = new HttpClient())
+            var customError = new List<error>
+        {
+            new error
             {
-                try
+                message = errorCode,
+                detail = new List<errorDetail>
                 {
-                    HttpResponseMessage message = await client.GetAsync(externalUrl);
-
-                    if (message.StatusCode == System.Net.HttpStatusCode.NotFound || items.Count() == 0)
+                    new errorDetail
                     {
-                        _custom_error.Add(new error
-                        {
-                            message = "404",
-                            detail = new List<errorDetail>()
-                            {
-                                new errorDetail
-                                {
-                                    propertyName = "Not Found",
-                                    message = "No readings found for the specified stationId"
-                                }
-                            }
-                        });
-
-                        return NotFound(_custom_error);
-                    }
-                    else if (message.StatusCode == System.Net.HttpStatusCode.InternalServerError)
-                    {
-                        _custom_error.Add(new error
-                        {
-                            message = "500",
-                            detail = new List<errorDetail>()
-                            {
-                                new errorDetail
-                                {
-                                    propertyName = "Internal server error",
-                                    message = ""
-                                }
-                            }
-                        });
-
-                        return new JsonResult(_custom_error);
-                    }
-                    else if (message.StatusCode == System.Net.HttpStatusCode.BadRequest)
-                    {
-                        _custom_error.Add(new error
-                        {
-                            message = "400",
-                            detail = new List<errorDetail>()
-                            {
-                                new errorDetail
-                                {
-                                    propertyName = "Invalid request",
-                                    message = "Details of invalid request property"
-                                }
-                            }
-                        });
-
-                        return BadRequest(_custom_error);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error: {message.StatusCode}");
+                        propertyName = errorMessage,
+                        message = detail
                     }
                 }
-                catch (HttpRequestException ex)
-                {
-                    _custom_error.Add(new error
-                    {
-                        message = "",
-                        detail = new List<errorDetail>()
-                            {
-                                new errorDetail
-                                {
-                                    propertyName = "",
-                                    message =  $"HTTP request error: {ex.Message}",
-                                }
-                            }
-                    });
-                    Console.WriteLine($"HTTP request error: {ex.Message}");
-                }
             }
-
-            
-            foreach (var item in items)
-            {
-                _readings_res.Add(new Models.rainfallReadingResponse
-                {
-                    Readings = new List<Models.rainfallReading>()
-                    {
-                        new Models.rainfallReading
-                        {
-                            dateMeasured = DateTime.Parse(item["dateTime"].ToString()),
-                            amountMeasured = decimal.Parse(item["value"].ToString()),
-                        }
-                    }
-                });
-            }
-            
-
-            return new JsonResult(_readings_res);
+        };
+            return customError;
         }
     }
+
 }
